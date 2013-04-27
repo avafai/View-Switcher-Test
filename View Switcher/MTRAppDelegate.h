00a524cedeb1c488390bad7f25859cd75cea4dfc
WECoyote <ADV@Millennium-Falcon.local>
2013-04-26 18:07:01 -0700

    Initial Commit

---
+++ View Switcher/MTRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MTRAppDelegate.h
+//  View Switcher
+//
+//  Created by Ali Vafai on 4/26/13.
+//  Copyright (c) 2013 Ali Vafai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MTRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ View Switcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  View Switcher
+//
+//  Created by Ali Vafai on 4/26/13.
+//  Copyright (c) 2013 Ali Vafai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MTRAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MTRAppDelegate class]));
+    }
+}
